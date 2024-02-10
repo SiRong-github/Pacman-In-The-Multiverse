e5256b962fc164b9975cd8058d4d97ce05d7ceb3
SiRong-github <62817554+SiRong-github@users.noreply.github.com>
2024-02-10 12:53:06 +0800

    Create MonsterFactory.java

---
+++ pacman/src/Monster/MonsterFactory.java
@@ -0,0 +1,24 @@
+package src.Monster;
+
+import src.Game;
+import src.Monster.*;
+
+public class MonsterFactory {
+    private final Game game;
+
+    public MonsterFactory(Game game) {
+        this.game = game;
+    }
+
+    public Monster createMonster(int index) {
+        Monster monster = switch (index) {
+            case 0 -> new Troll(game);
+            case 1 -> new TX5(game);
+            case 2 -> new Orion(game);
+            case 3 -> new Alien(game);
+            case 4 -> new Wizard(game);
+            default -> null;
+        };
+        return monster;
+    }
+}
