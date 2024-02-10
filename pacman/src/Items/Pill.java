15b336de01bb68ce90b9ba26cdf380d4a6c7cee0
SiRong-github <62817554+SiRong-github@users.noreply.github.com>
2024-02-10 12:31:27 +0800

    Updated Pill.java to correct file

--- pacman/src/Items/Pill.java
+++ pacman/src/Items/Pill.java
@@ -1,13 +1,17 @@
-package src.Items;
+package src;
 
 import ch.aplu.jgamegrid.Location;
 import src.Items.Item;
 import src.Items.ItemType;
 
-public class Pill extends Item {
-    private static ItemType itemType = ItemType.PILL;
+public class Pill {
+    private Location location;
 
     public Pill(Location location) {
-        super(location, itemType);
+        this.location = location;
+    }
+
+    public Location getLocation() {
+        return location;
     }
 }
